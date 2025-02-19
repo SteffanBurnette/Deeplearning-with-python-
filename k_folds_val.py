74062244119b019bf4d566b4daf5695c41432075
Steffan Burnette <burnettesteffansep7@gmail.com>
2025-02-19 17:39:13 -0500

    k folds

---
+++ k_folds_val.py
@@ -0,0 +1,23 @@
+import numpy as np
+#K-fold Cross Validation
+k = 4
+num_val_samples = len(train_data) // k
+num_epochs = 500
+all_scores = []
+for i in range(k):
+ print(f"Processing fold #{i}")
+ val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+ val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+ partial_train_data = np.concatenate(
+ [train_data[:i * num_val_samples],
+ train_data[(i + 1) * num_val_samples:]],
+ axis=0)
+ partial_train_targets = np.concatenate(
+ [train_targets[:i * num_val_samples],
+ train_targets[(i + 1) * num_val_samples:]],
+ axis=0)
+ model = build_model()
+model.fit(partial_train_data, partial_train_targets,
+ epochs=num_epochs, batch_size=16, verbose=0)
+val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
+all_scores.append(val_mae)
